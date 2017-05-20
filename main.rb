a7ab454fc135c957a78fd37268773b090ea81dce
Suckovich <iwill@nottellyou.myemail>
2017-05-21 02:27:55 +0300

    copypasted

---
+++ main.rb
@@ -0,0 +1,13 @@
+# config.ru
+
+require 'telegram'
+
+Telegram.token = ENV['TELEGRAM_TOKEN']
+
+class App < Bot
+  on :echo do |update|
+    update.message.chat.reply(update.message.text)
+  end
+end
+
+run App.new
